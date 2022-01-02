2e5345260c9c4c9f541975e5393b5294d643551a
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-02 14:08:57 +0100

    chore: remove unused env var

--- environment.d.ts
+++ environment.d.ts
@@ -5,7 +5,6 @@ namespace NodeJS {
     DISCORD_BOT_TOKEN: string;
     DISCORD_USER_ID: string;
     GITHUB_USER_ID: string;
-    CONTACT_EMAIL_ADDRESS: string;
     NEXT_PUBLIC_EMAILJS_USER_ID: string;
     NEXT_PUBLIC_EMAILJS_SERVICE_ID: string;
     NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: string;
